d04393cd0e64ebe5842cc294a8ff2aeb8baceb7d
kuliang <kuliang@ucsd.edu>
2018-11-17 14:46:51 -0800

    topbar

---
+++ src/components/TopBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from "../assets/images/logo.svg"
+export class TopBar extends React.Component {
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                </header>
+            </div>
+        );
+    }
+}
