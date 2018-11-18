3c544ad00a5beccaf9967dc2221c1ecda6048a75
kuliang <kuliang@ucsd.edu>
2018-11-18 14:09:58 -0800

    move chart up

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import logo from '../assets/images/logo.svg';
-import '../styles/App.css';
-import {TopBar} from "./TopBar"
+import { TopBar } from './TopBar';
+import { Main } from './main';
 
 class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <TopBar/>
-        </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <TopBar/>
+                <Main/>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from "../assets/images/logo.svg"
+import logo from '../assets/images/logo.svg';
+
 export class TopBar extends React.Component {
     render() {
         return (
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                </header>
-            </div>
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+            </header>
         );
     }
 }
+
